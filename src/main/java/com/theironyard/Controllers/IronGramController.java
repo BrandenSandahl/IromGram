1711eb24efd4cf16761a2f5e53ecf189ac795434
BrandenSandahl <branden157@gmail.com>
2016-03-15 12:01:50 -0400

    finished login route

--- src/main/java/com/theironyard/Controllers/IronGramController.java
+++ src/main/java/com/theironyard/Controllers/IronGramController.java
@@ -1,5 +1,7 @@
 package com.theironyard.controllers;
 
+import com.sun.deploy.net.HttpResponse;
+import com.theironyard.Utils.PasswordStorage;
 import com.theironyard.entities.User;
 import com.theironyard.services.PhotoRepository;
 import com.theironyard.services.UserRepository;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.List;
@@ -41,10 +44,18 @@ public class IronGramController {
 
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public User login() {
+    public User login(String userName, String password, HttpSession session, HttpServletResponse response) throws Exception {
+        User user = userRepository.findByName(userName);
+        if (user == null) {
+            user = new User(userName, PasswordStorage.createHash(password));
+            userRepository.save(user);
+        } else if (!PasswordStorage.verifyPassword(password, user.getPasswordHash())){
+            throw new Exception("Wrong Password");
+        }
 
-
-        return null;
+        session.setAttribute("userName", userName);
+        response.sendRedirect("/");  //this redirects. may not need this for front end projects
+        return user;
     }
 
     @RequestMapping(path = "/user", method = RequestMethod.GET)
